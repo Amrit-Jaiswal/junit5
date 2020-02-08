a4c9f817c3de4ecb865069f894c1e05a812a3108
Amrit <jaiswal.amrit@gmail.com>
2020-02-08 17:07:34 +0530

    Test case for exception added

---
+++ src/test/java/com/example/ExceptionTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionTest {
+
+    @Test
+    public void testExpectedException(){
+        Assertions.assertThrows(NumberFormatException.class, () -> {
+            Integer.parseInt("One");
+        });
+    }
+
+    @Test
+    public void testExpectedExceptionWithSuperType(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+            Integer.parseInt("One");
+        });
+    }
+
+    @Test
+    public void testExpectedExceptionFail(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            Integer.parseInt("1");
+        });
+    }
+}
